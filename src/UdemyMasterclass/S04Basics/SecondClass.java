227d0e905811fc07cf1f5e92d335567c76ac6093
pedronx <pedro.ludgero@gmail.com>
2022-12-14 16:24:17 +0000

    Second Class

---
+++ src/UdemyMasterclass/S04Basics/SecondClass.java
@@ -0,0 +1,9 @@
+package UdemyMasterclass.S04Basics;
+
+public class SecondClass {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Hello, Tim!");
+    }
+}
