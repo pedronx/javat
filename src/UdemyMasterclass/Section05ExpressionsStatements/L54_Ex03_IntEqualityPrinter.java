0648cd8da3590f999723ca2b977669c2ab91a4e9
pedronx <pedro.ludgero@gmail.com>
2022-12-20 14:46:36 +0000

    Section 5 | Lessons 54 Exercises 01 to 04

---
+++ src/UdemyMasterclass/Section05ExpressionsStatements/L54_Ex03_IntEqualityPrinter.java
@@ -0,0 +1,45 @@
+package UdemyMasterclass.Section05ExpressionsStatements;
+
+/**
+ * Equality Printer
+ * Write a method printEqual with 3 parameters of type int.
+ * The method should not return anything (void).
+ * If one of the parameters is less than 0, print text "Invalid Value".
+ * If all numbers are equal print text"All numbers are equal"
+ * If all numbers are different print text "All numbers are different".
+ * Otherwise, print "Neither all are equal or different".
+ * >>>
+ * EXAMPLES OF INPUT/OUTPUT:
+ *    . printEqual(1, 1, 1); should print text "All numbers are equal"
+ *    . printEqual(1, 1, 2); should print text "Neither all are equal or different"
+ *    . printEqual(-1, -1, -1); should print text "Invalid Value"
+ *    . printEqual(1, 2, 3); should print text "All numbers are different"
+ */
+public class L54_Ex03_IntEqualityPrinter {
+
+    public static void main(String[] args) {
+        printEqual(1, 1, 1);
+        printEqual(1, 1, 2);
+        printEqual(-1, -1, -1);
+        printEqual(1, 2, 3);
+    }
+
+    /**
+     * If one of the parameters is less than 0, print text "Invalid Value".
+     * If all numbers are equal print text"All numbers are equal"
+     * If all numbers are different print text "All numbers are different".
+     * Otherwise, print "Neither all are equal or different".
+     */
+    public static void printEqual(int numberOne, int numberTwo, int numberThree) {
+
+        if (numberOne < 0 || numberTwo < 0 || numberThree < 0) {
+            System.out.println("Invalid Value");
+        } else if (numberOne == numberTwo && numberTwo == numberThree) {
+            System.out.println("All numbers are equal");
+        } else if (numberOne == numberTwo || numberOne == numberThree || numberTwo == numberThree) {
+            System.out.println("Neither all are equal or different");
+        } else {
+            System.out.println("All numbers are different");
+        }
+    }
+}
---
+++ src/UdemyMasterclass/Section05ExpressionsStatements/L54_Ex04_PlayingCat.java
@@ -0,0 +1,47 @@
+package UdemyMasterclass.Section05ExpressionsStatements;
+
+/**
+ * The cats spend most of the day playing.
+ * In particular, they play if the temperature is between 25 and 35 (inclusive).
+ * Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+ * >>>
+ * Write a method isCatPlaying that has 2 parameters.
+ * Method needs tn true if the cat is playing, otherwise return false
+ *    . 1st parameter should be of type boolean and be named summer it represents if it is summer.
+ *    . 2nd parameter represents the temperature and is of type int with the name temperature.
+ * >>>
+ * EXAMPLES OF INPUT/OUTPUT:
+ *    . isCatPlaying(true, 10); should return false since temperature is not in range 25 - 45
+ *    . isCatPlaying(false, 36); should return false since temperature is not in range 25 - 35 (summer parameter is false)
+ *    . isCatPlaying(false, 35); should return true since temperature is in range 25 - 35
+ */
+public class L54_Ex04_PlayingCat {
+
+    public static void main(String[] args) {
+        System.out.println("isCatPlaying(true, 10) : " + isCatPlaying(true, 10));
+        System.out.println("isCatPlaying(false, 36) : " + isCatPlaying(false, 36));
+        System.out.println("isCatPlaying(false, 35) : " + isCatPlaying(false, 35));
+        System.out.println("isCatPlaying(false, 25) : " + isCatPlaying(false, 25));
+        System.out.println("isCatPlaying(false, 24) : " + isCatPlaying(false, 24));
+        System.out.println("isCatPlaying(true, 46) : " + isCatPlaying(true, 45));
+        System.out.println("isCatPlaying(true, 46) : " + isCatPlaying(true, 46));
+    }
+
+    /**
+     * Method returns true if the cat is playing, otherwise return false
+     * Is plays if the temperature is between 25 and 35 (inclusive).
+     * Unless it is summer, then the upper limit is 45 (inclusive) instead of 35.
+     */
+    public static boolean isCatPlaying(boolean summer, int temperature) {
+
+        if (summer) {
+            if (temperature >= 25 && temperature <= 45) {
+                return true;
+            }
+        } else if (temperature >= 25 && temperature <= 35) {
+            return true;
+        }
+
+        return false;
+    }
+}
