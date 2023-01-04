059c5b8a69d04129aff66c9f31ba01cce1c5bdc0
pedronx <pedro.ludgero@gmail.com>
2023-01-04 15:07:47 +0000

    Section 6 | Lessons 74 (all exercises complete)

--- src/UdemyMasterclass/Section06ControlFlow/L74_Ex29_PaintJob.java
+++ src/UdemyMasterclass/Section06ControlFlow/L74_Ex29_PaintJob.java
@@ -58,5 +58,65 @@ public class L74_Ex29_PaintJob {
 
     public static void main (String[] args) {
 
+        System.out.println("getBucketCount(-3.4, 2.1, 1.5, 2) = " + getBucketCount(-3.4, 2.1, 1.5, 2));
+        System.out.println("getBucketCount0, 2.1, 1.5, 2) = " + getBucketCount(0, 2.1, 1.5, 2));
+        System.out.println("getBucketCount(3.4, 2.1, 1.5, 2) = " + getBucketCount(3.4, 2.1, 1.5, 2));
+        System.out.println("getBucketCount(1.4, 2.1, 1.5, 2) = " + getBucketCount(1.4, 2.1, 1.5, 2));
+        System.out.println("getBucketCount(2.75, 3.25, 2.5, 1) = " + getBucketCount(2.75, 3.25, 2.5, 1));
+        System.out.println(">>>");
+        System.out.println();
+        System.out.println("getBucketCount(-3.4, 2.1, 1.5) = " + getBucketCount(-3.4, 2.1, 1.5));
+        System.out.println("getBucketCount(0, 2.1, 1.5) = " + getBucketCount(0, 2.1, 1.5));
+        System.out.println("getBucketCount(3.4, 2.1, 1.5) = " + getBucketCount(3.4, 2.1, 1.5));
+        System.out.println("getBucketCount(7.25, 4.3, 2.35) = " + getBucketCount(7.25, 4.3, 2.35));
+        System.out.println();
+        System.out.println("getBucketCount(-3.4, 1.5) = " + getBucketCount(-3.4, 1.5));
+        System.out.println("getBucketCount(0, 1.5) = " + getBucketCount(0, 1.5));
+        System.out.println("getBucketCount(3.4, 1.5) = " + getBucketCount(3.4, 1.5));
+        System.out.println("getBucketCount(6.26, 2.2) = " + getBucketCount(6.26, 2.2));
+        System.out.println("getBucketCount(3.26, 0.75) = " + getBucketCount(3.26, 0.75));
     }
+
+
+    public static int getBucketCount(double width, double height, double areaPerBucket, int extraBuckets) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0 || extraBuckets < 0) {
+            return -1;
+        }
+
+        // Calculate the area of the wall (A = w * h)
+        double areaWall = width * height;
+
+        // calculate how many area the extraBuckets will cover
+        areaWall -= (extraBuckets * areaPerBucket);
+
+        if (areaWall <= 0) {
+            return 0;
+        }
+
+        return ((int)Math.ceil(areaWall / areaPerBucket));
+    }
+
+    public static int getBucketCount(double width, double height, double areaPerBucket) {
+
+        if (width <= 0 || height <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        // Calculate the area of the wall (A = w * h)
+        double areaWall = width * height;
+
+        return ((int)Math.ceil(areaWall / areaPerBucket));
+    }
+
+    public static int getBucketCount(double area, double areaPerBucket) {
+
+        if (area <= 0 || areaPerBucket <= 0) {
+            return -1;
+        }
+
+        return ((int)Math.ceil(area / areaPerBucket));
+    }
+
+
 }
