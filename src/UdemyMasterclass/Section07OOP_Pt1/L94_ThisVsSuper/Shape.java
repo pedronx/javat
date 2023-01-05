1be92fdd89fae574f570fd3e3688c3ee646d8326
pedronx <pedro.ludgero@gmail.com>
2023-01-05 22:51:36 +0000

    Section 7 | Lesson 94

--- src/UdemyMasterclass/Section07OOP_Pt1/L94_ThisVsSuper/Shape.java
+++ src/UdemyMasterclass/Section07OOP_Pt1/L94_ThisVsSuper/Shape.java
@@ -12,18 +12,18 @@ class Shape {
     }
 }
 
-class Rectangle extends Shape {
+class AnotherRectangle extends Shape {
 
     private int width;
     private int height;
 
     // 1st constructor
-    public Rectangle(int x, int y) {
+    public AnotherRectangle(int x, int y) {
         this(x, y, 0, 0); // calls 2nd constructor
     }
 
     // 2nd constructor
-    public Rectangle(int x, int y, int width, int height) {
+    public AnotherRectangle(int x, int y, int width, int height) {
         super(x, y); // calls constructor from parent (Shape)
         this.width = width;
         this.height = height;
