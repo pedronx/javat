aaaa966902bf485767b4b0f6574808d6f45c0d2f
pedronx <pedro.ludgero@gmail.com>
2023-01-06 17:29:35 +0000

    Section 7 | Lessons 98 to 99 (including exercises)

---
+++ src/UdemyMasterclass/Section07OOP_Pt1/L99_Ex36_Cylinder/Cylinder.java
@@ -0,0 +1,24 @@
+package UdemyMasterclass.Section07OOP_Pt1.L99_Ex36_Cylinder;
+
+public class Cylinder extends Circle {
+
+    private double height;
+
+    public Cylinder(double radius, double height) {
+        super(radius);
+
+        if (height < 0) {
+            this.height = 0.0;
+        } else {
+            this.height = height;
+        }
+    }
+
+    public double getHeight() {
+        return height;
+    }
+
+    public double getVolume() {
+        return this.getArea() * this.height;
+    }
+}
