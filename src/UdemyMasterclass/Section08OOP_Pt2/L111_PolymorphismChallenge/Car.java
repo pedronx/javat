3f0247d75c1e9446755d8ed00033c1f0e9f75ac5
pedronx <pedro.ludgero@gmail.com>
2023-01-07 18:32:26 +0000

    Section 7 | Lesson 109 to 111

---
+++ src/UdemyMasterclass/Section08OOP_Pt2/L111_PolymorphismChallenge/Car.java
@@ -0,0 +1,27 @@
+package UdemyMasterclass.Section08OOP_Pt2.L111_PolymorphismChallenge;
+
+public class Car {
+
+    private String description;
+
+    public Car(String description) {
+        this.description = description;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void startEngine() {
+        System.out.println("Car -> startEngine");
+    }
+
+    public void drive() {
+        System.out.println("Car -> driving, type is " + getClass().getSimpleName());
+        runEngine();
+    }
+
+    protected void runEngine() {
+        System.out.println("Car -> runEngine");
+    }
+}
---
+++ src/UdemyMasterclass/Section08OOP_Pt2/L111_PolymorphismChallenge/HybridCar.java
@@ -0,0 +1,19 @@
+package UdemyMasterclass.Section08OOP_Pt2.L111_PolymorphismChallenge;
+
+public class HybridCar extends Car{
+
+    private double avgKmPerLitre;
+    private int batterySize;
+    private int cylinders;
+
+    public HybridCar(String description) {
+        super(description);
+    }
+
+    public HybridCar(String description, double avgKmPerLitre, int batterySize, int cylinders) {
+        super(description);
+        this.avgKmPerLitre = avgKmPerLitre;
+        this.batterySize = batterySize;
+        this.cylinders = cylinders;
+    }
+}
