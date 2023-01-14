2f9344eb31d0b631ae40222f95057ebc9fe1d0ce
pedronx <pedro.ludgero@gmail.com>
2023-01-14 19:05:30 +0000

    Section 10 | Lesson 135 (complete)

--- src/UdemyMasterclass/Section10Lists/L135_ArrayListChallenge/Main.java
+++ src/UdemyMasterclass/Section10Lists/L135_ArrayListChallenge/Main.java
@@ -1,6 +1,7 @@
 package UdemyMasterclass.Section10Lists.L135_ArrayListChallenge;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -24,9 +25,40 @@ public class Main {
 
     public static void main (String[] args) {
 
-        int menuOption = getMenuFromScanner();
-        ArrayList<String> groceryList = getArrayListFromScanner();
+        int menuOption = 0;
+        boolean exitApp = false;
+        ArrayList<String> groceryList = new ArrayList<>();
 
+        do {
+            // displays Main menu and validates if valid input
+            menuOption = getMenuFromScanner();
+
+            switch (menuOption) {
+                case 0:
+                    System.out.println("Thank you for using the application.\nCome again!...");
+                    exitApp = true;
+                    break;
+                case 1:
+                    System.out.print("Insert the values (comma delimited list): ");
+                    ArrayList<String> scannerList = getArrayListFromScanner();
+                    groceryList = insertItems(groceryList, scannerList);
+
+                    System.out.print("List: " + groceryList + "\n\n");
+                    System.out.print("Press 'Enter' to continue...");
+                    getArrayListFromScanner(); // waiting for any key to be pressed
+                    System.out.println(System.lineSeparator().repeat(50)); // clears the screen
+                    break;
+                case 2:
+                    System.out.print("Insert the values (comma delimited list): ");
+                    ArrayList<String> removalList = getArrayListFromScanner();
+                    groceryList = removeItems(groceryList, removalList);
+                    System.out.print("List: " + groceryList + "\n\n");
+                    System.out.print("Press 'Enter' to continue...");
+                    getArrayListFromScanner(); // waiting for any key to be pressed
+                    System.out.println(System.lineSeparator().repeat(50)); // clears the screen
+                    break;
+            }
+        } while (!exitApp);
     }
 
 
@@ -44,19 +76,20 @@ public class Main {
 
             // catch the exceptions if user enters characters
             try {
-                menu = checkData(menuOption);
+                menu = checkMenuData(menuOption);
 
                 switch(menu) {
                 case 0, 1, 2:
                     validDOB = true;
                     break;
                 default:
-                    System.out.println("Invalid Value! PLease try again.");
+                    System.out.println(System.lineSeparator().repeat(50)); // clears the screen
+                    System.out.println("Invalid value! PLease try again.\n");
                     break;
                 };
             } catch (NumberFormatException badUserData) {
-                System.out.println("Invalid input! Please try again.");
                 System.out.println(System.lineSeparator().repeat(50)); // clears the screen
+                System.out.println("Invalid input! Please try again.\n");
             }
         } while (!validDOB);
 
@@ -66,7 +99,7 @@ public class Main {
     }
 
     // Validate data entered by user
-    public static int checkData(String menuOption) {
+    public static int checkMenuData(String menuOption) {
         int menu = Integer.parseInt(menuOption);
 
         if (menu < 0 || menu > 3) {
@@ -77,27 +110,67 @@ public class Main {
     }
 
     public static void displayMenu() {
-        System.out.print(" *   0 - to shut down\n" +
+
+        System.out.print(" *   Available actions:\n" +
+                " *\n" +
+                " *   0 - to shut down\n" +
+                " *\n" +
                 " *   1 - to add item(s) to list (comma delimited list)\n" +
-                " *   3 - to remove any items (comma delimited list)\n" +
+                " *\n" +
+                " *   2 - to remove any items (comma delimited list)\n" +
+                " *\n" +
                 " *   Enter a number for which action you want to perform: ");
     }
 
-    public static void insertItems() {
+    // Receives 1 original list and 1 List with new elements
+    // Inserts new elements (verifies if duplicates) and sorts
+    public static ArrayList<String> insertItems(ArrayList<String> groceryList, ArrayList<String> scannerList) {
 
+        System.out.print("Input: ");
+        for (int i = 0; i < scannerList.size(); i++) {
+
+            System.out.print(scannerList.get(i) + " ");
+            if (scannerList.get(i).isBlank() || scannerList.get(i).isEmpty() || groceryList.contains(scannerList.get(i))) {
+                continue;
+            } else {
+
+                groceryList.add(scannerList.get(i).trim());
+            }
+        }
+        System.out.println();
+        groceryList.sort(Comparator.naturalOrder());
+
+        return groceryList;
     }
 
-    public static void removeItems() {
-        // Instantiate a scanner object
-        Scanner scanner = new Scanner(System.in);
+    // Receives 1 original list and 1 List with elements to remove
+    // Removes elements from the original list
+    public static ArrayList<String> removeItems(ArrayList<String> groceryList, ArrayList<String> removalList) {
 
-        System.out.print("Insert the item(s) (comma delimited list): ");
+        System.out.print("Input: ");
+        for (int i = 0; i < removalList.size(); i++) {
+
+            System.out.print(removalList.get(i) + " ");
+
+            if (groceryList.contains(removalList.get(i).trim())) {
+                groceryList.remove(removalList.get(i).trim());
+            }
+        }
+        System.out.println();
+        groceryList.sort(Comparator.naturalOrder());
+
+        return groceryList;
     }
 
     public static ArrayList<String> getArrayListFromScanner() {
 
-        ArrayList<String> groceryList = new ArrayList<>((List.of("Test")));
+        // Instantiate a scanner object
+        Scanner scanner = new Scanner(System.in);
 
-        return groceryList;
+        String items = scanner.nextLine();
+
+        ArrayList<String> scannerList = new ArrayList<>((List.of(items.split(","))));
+
+        return scannerList;
     }
 }
