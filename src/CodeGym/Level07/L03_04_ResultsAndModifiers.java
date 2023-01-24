ea898ca0be97322ef3efb66e1f1094ae0b23c37c
pedronx <pedro.ludgero@gmail.com>
2023-01-24 00:34:56 +0000

    Code Gym | Level 7 & 8 complete

--- src/CodeGym/Level07/L03_04_ResultsAndModifiers.java
+++ src/CodeGym/Level07/L03_04_ResultsAndModifiers.java
@@ -4,5 +4,113 @@ public class L03_04_ResultsAndModifiers {
 
     public static void main (String[] args) {
 
+        // Lesson 03 | Task 01
+//        signIn("PedroNx");
+//        signIn("user");
+
+        // Lesson 03 | Task 02
+//        System.out.println(cube(56423));
+
+        // Lesson 03 | Task 03
+//        System.out.println(ninthDegree(56));
+
+        // Lesson 04 | Task 02
+        int[] array = {123, 7, -14, 67, 16, 98, 75, 576};
+        System.out.println("In the array: ");
+        printArray(array);
+        System.out.println("Minimum: " + getMinValue(array));
+        System.out.println("Maximum: " + getMaxValue(array));
+    }
+
+    /**
+     * Welcome! But not everyone.
+     * This is the signIn() method that greets website users. Currently, it greets all users, but it should only greet registered users. All unregistered users have the name "user".
+     * Add username validation at the beginning of the signIn() method.
+     * If the name is "user", use the return keyword to abort execution of the method.
+     * Requirements:
+     *   . If the username is "user", then the signIn() method should exit without printing anything on the console.
+     *   . If the username is not "user", then the signIn() method should continue and print greetings on the console.
+     *   . The return keyword must be used to interrupt the signIn() method.
+     */
+    public static void signIn(String username) {
+
+        if ( username == "user") {
+            return;
+        }
+        else {
+            System.out.println("Welcome, " + username);
+            System.out.println("We've really missed you, " + username);
+        }
+    }
+
+    /**
+     * A cube calculator
+     * Let's implement a calculator that will compute the powers of numbers.
+     * To do this, create a cube() method. It must take a long integer value as an argument.
+     * The method should raise the passed value to the third power and return it as the method's result.
+     * The numbers you have to work with may be large. Accordingly, the cube() method must return a long.
+     * Requirements:
+     *   . There must be a public static long cube(long) method.
+     *   . The cube() method must return the result of raising the passed number to the power of three.
+     */
+    public static long cube(long number) {
+
+        return number * number * number;
+    }
+
+    /**
+     * A cubed cube calculator
+     * You won't blow any minds by unveiling a calculator that can raise a number to the power of three.
+     * But a calculator that can raise a number to the power of nine is another matter! So let's implement one!
+     * >>>
+     * To do this, create a public static long ninthDegree(long) method.
+     * It must take a long integer value as an argument.
+     * The method should raise the passed value to the ninth power and return it as the method's result.
+     * You'll remember from your high school math class that you can get the ninth power of a number if you raise it to the third power and then raise the result to the third power. Let's use this rule in our program.
+     * In the ninthDegree() method, raise the passed argument to the third power using the cube() method.
+     * Then pass the result to the cube() method once again. The final result is the method's return value.
+     * Requirements:
+     *   . There must be a public static long ninthDegree(long a) method.
+     *   . The ninthDegree() method must return the result of raising the passed number to the power of nine.
+     *   . The ninthDegree() method must use the cube() method to successively cube the number.
+     */
+    public static long ninthDegree(long number) {
+
+        return cube(cube(number));
+    }
+
+    /**
+     * Analyzing arrays
+     * This program should display information about the created array. But due to misplaced static modifiers, it won't compile.
+     * Correct these mistakes. Add the static modifier where it is needed.
+     * Requirements:
+     *   . The code must compile.
+     *   . Add the minimum required number of static modifiers.
+     */
+    public static int getMaxValue(int[] array) {
+        int max = Integer.MIN_VALUE;
+        for (int value : array) {
+            if (value > max) {
+                max = value;
+            }
+        }
+        return max;
+    }
+
+    public static int getMinValue(int[] array) {
+        int min = Integer.MAX_VALUE;
+        for (int value : array) {
+            if (value < min) {
+                min = value;
+            }
+        }
+        return min;
+    }
+
+    public static void printArray(int[] array) {
+        for (int i : array) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
--- src/CodeGym/Level07/L05_06_Variables.java
+++ src/CodeGym/Level07/L05_06_Variables.java
@@ -2,7 +2,132 @@ package CodeGym.Level07;
 
 public class L05_06_Variables {
 
+    /*--- Lesson 5 | Task 02 ---*/
+    public static String city = "Tokyo";
+    public static double population = 34.5;
+
+    /*--- Lesson 6 | Task 01 ---*/
+    public static final double pi = 3.14;
+    public static double radius = 2.5;
+    public static final double diameter = 5;
+
+    /*--- Lesson 6 | Task 03 ---*/
+    public String name = "Amigo";
+    public String position = "Java developer";
+    public int salary = 10_000;
+
     public static void main (String[] args) {
 
+        // Lesson 05 | Task 01
+//        int[] array = {15, 64, 9, 51, 42};
+//        printSqrt(array);
+
+        // Lesson 05 | Task 02
+//        printCityPopulation("Jakarta", 25.3);
+//        printCityPopulation("Seoul", 25.2);
+//        printCityPopulation("Delhi", 23.1);
+//        printCityPopulation("New York City", 21.6);
+
+        // Lesson 06 | Task 01
+        areaAndPerimeter();
     }
+
+    /**
+     * Everything has a root
+     * The printSqrt(int[] array) method should print the square root of each element in the passed array.
+     * But this isn't happening due to conflicting variable names. Correct the variable names so that the code compiles.
+     * The program should display a suitable line on the console for each element of the array.
+     * Example of a line:
+     * The square root of 64 is 8.0
+     * Requirements:
+     *   . The code must compile.
+     *   . For each element in the array, the printSqrt(int[] array) method must print a string in accordance with the task conditions.
+     */
+    public static void printSqrt(int[] array) {
+        String squareText = "The square root of ";
+        for (int i = 0; i < array.length; i++) {
+            int element = array[i];
+            double elementSqrt = Math.sqrt(element);
+            System.out.println(squareText + element + " is " + elementSqrt);
+        }
+    }
+
+    /**
+     * Giant cities
+     * The program should display the population of the largest cities in the world
+     * and compare them with Tokyo, the most populous city of all.
+     * But the program's algorithm is slightly broken. To correct the error,
+     * you need to make it so the line that displays information about the world's largest city
+     * uses static class variables instead of local method arguments.
+     * >>>
+     * When the program is working correctly, the output should be as follows:
+     * The population of Jakarta is 25.3 million.
+     * But in the most populous city, Tokyo, the population is 34.5 million.
+     * The population of Seoul is 25.2 million.
+     * But in the most populous city, Tokyo, the population is 34.5 million.
+     * The population of Delhi is 23.1 million.
+     * But in the most populous city, Tokyo, the population is 34.5 million.
+     * The population of New York City is 21.6 million.
+     * But in the most populous city, Tokyo, the population is 34.5 million.
+     * Requirements:
+     *   . You cannot change the names of the variables.
+     *   . The class's static city and population variables should be used to display information about the most populous city.
+     *   . The program's console output must match the task conditions.
+     */
+    public static void printCityPopulation(String city, double population) {
+        System.out.println("The population of " + city + " is " + population + " million.");
+        System.out.println("But in the most populous city, " + L05_06_Variables.city
+                + ", the population is " + L05_06_Variables.population + " million.");
+    }
+
+    /**
+     * Remembering geometry
+     * Take a look at what the program does. Add the final modifier to standard constants (variables with immutable values).
+     * Requirements:
+     *   . The final modifier must be set on all constants.
+     */
+    public static void areaAndPerimeter() {
+        System.out.println("The area of the circle is: " + pi * radius * radius);
+        System.out.println("The perimeter of the circle is: " + pi * diameter);
+    }
+
+    /**
+     * Everything you need to know about Earth
+     * Here is the Earth class — a collection of variables that describe certain characteristics of the planet Earth.
+     * Be sure that the variables of the Earth class are global constants.
+     * Don't forget to rename the variables so that they comply with code style conventions for global variables.
+     * Requirements:
+     *   . The Earth class's name variable needs to be changed into a global variable.
+     *   . The Earth class's square variable needs to be changed into a global variable.
+     *   . The Earth class's population variable needs to be changed into a global variable.
+     *   . The Earth class's equatorLength variable needs to be changed into a global variable.
+     */
+    public class Earth {
+        public static final String NAME = "Earth";
+        public static final double SQUARE = 510_100_000.0;
+        public static final long POPULATION = 7_594_000_000L;
+        public static final long EQUATOR_LENGTH = 40_075_696L;
+    }
+
+    /**
+     * Employee records
+     * Here you have in front of you a digital employee record. It contains the employee's name, position and salary.
+     * The name of the employee is unlikely to change, but his or her position and salary might.
+     * These changes are made using the setPosition() and setSalary() methods. Of course, they are not working correctly at present.
+     * Figure out the reason and fix the bugs. In doing this, neither the names of instance variables, nor the names of method parameters can be changed.
+     * Requirements:
+     *   . The setPosition() method must set the passed value to the position instance variable.
+     *   . The setSalary() method must set the passed value to the salary instance variable.
+     *   . You cannot change the names of the instance variables.
+     *   . You cannot change the names of method parameters.
+     */
+    public void setPosition(String position) {
+        this.position = position;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+
 }
